31925cf0531a4457aa34c8e37562eec724369408
Artyom <artyom_kudryavcev_91@mail.ru>
2022-11-30 00:01:13 +0700

    added algorithm in project Task3

--- Seminar1/Example2/Task3/Task3/Program.cs
+++ Seminar1/Example2/Task3/Task3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите номер дня недели: ");
+int numberDay = int.Parse(Console.ReadLine());
+if (numberDay < 0 | numberDay > 7)
+{
+Console.WriteLine("Введите корректное число");
+}
+if (numberDay == 1)
+{
+Console.WriteLine("Понедельник");
+}
+if (numberDay == 2)
+{
+Console.WriteLine("Вторник");
+}
+if (numberDay == 3)
+{
+Console.WriteLine("Среда");
+}
+if (numberDay == 4)
+{
+Console.WriteLine("Четверг");
+}
+if (numberDay == 5)
+{
+Console.WriteLine("Пятница");
+}
+if (numberDay == 6)
+{
+Console.WriteLine("Суббота");
+}
+if (numberDay == 7)
+{
+Console.WriteLine("Воскресенье");
+}
