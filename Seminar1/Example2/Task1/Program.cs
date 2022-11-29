cb1790b516960051efc951acd70a494f3cf65a81
Artyom <artyom_kudryavcev_91@mail.ru>
2022-11-29 23:11:25 +0700

    added in project Task1 algoritm

--- Seminar1/Example2/Task1/Program.cs
+++ Seminar1/Example2/Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int square = number1*number1;
+if(number2 == square)
+{
+    Console.WriteLine("Второе число является квадратом первого числа!");
+}
+else {
+     Console.WriteLine("Второе число не является квадратом первого числа!");
+}
