fb4c35ea8e887333b55f88377367c3f641350550
Artyom <artyom_kudryavcev_91@mail.ru>
2022-11-30 23:16:23 +0700

    completed the project HomeWork1_Task4

--- HomeWork1/HomeWork1_Task4/Program.cs
+++ HomeWork1/HomeWork1_Task4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+int number_max = number1;
+
+
+if (number2 > number_max)
+{
+    number_max = number2;
+}
+if (number3 > number_max)
+{
+    number_max = number3;
+}
+Console.WriteLine ("Максимальное число " + number_max);
