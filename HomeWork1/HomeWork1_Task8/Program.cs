7e7f4cff378d3ada849da4260bc92babe15ac719
Artyom <artyom_kudryavcev_91@mail.ru>
2022-11-30 23:49:15 +0700

    completed the project HomeWork1_Task8

--- HomeWork1/HomeWork1_Task8/Program.cs
+++ HomeWork1/HomeWork1_Task8/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 0;
+while (number <= 1)
+{
+   Console.Write("Введите число: ");
+   number = int.Parse(Console.ReadLine());
+   if (number <= 1)
+   {
+    Console.WriteLine ("Введите число больше 1");
+   } 
+}
+
+for (int i = 2; i <= number; i++)
+{
+    if (i%2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
