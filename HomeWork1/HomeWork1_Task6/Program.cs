68892f6468e085f11ca8363e01acebf6bc8d059a
Artyom <artyom_kudryavcev_91@mail.ru>
2022-11-30 23:25:35 +0700

    completed the project HomeWork1_Task6

--- HomeWork1/HomeWork1_Task6/Program.cs
+++ HomeWork1/HomeWork1_Task6/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+
+if (number%2 == 0)
+{
+    Console.WriteLine ("Число " + number + " четное");
+}
+else
+{
+    Console.WriteLine ("Число " + number + " не четное");
+}
