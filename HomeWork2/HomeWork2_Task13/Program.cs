baad138cad6539d9a33b409528d751be94f3d520
Artyom <artyom_kudryavcev_91@mail.ru>
2022-12-07 01:05:54 +0700

    completed the project HomeWork2_Task13

--- HomeWork2/HomeWork2_Task13/Program.cs
+++ HomeWork2/HomeWork2_Task13/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number_user = 0;
+int result = 0;
+while (number_user < 100)
+{
+ Console.Write("Введите число от 100: ");
+ number_user = int.Parse(Console.ReadLine());
+ if(number_user < 100)
+ {
+    Console.WriteLine ("Вы ввели число менее 100 ");
+
+ }
+}
+int div = 10;
+while ((number_user / div) > 999)
+{
+    div = div*10;
+}
+if (number_user < 1000)
+{
+  result = number_user%10;  
+}
+else 
+{
+ number_user = number_user/div;
+ result = number_user%10;
+}
+Console.WriteLine(result);
