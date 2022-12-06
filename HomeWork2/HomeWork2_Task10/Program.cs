5b19565b1d511ad31d5dd15b095bb132ce44713d
Artyom <artyom_kudryavcev_91@mail.ru>
2022-12-06 22:17:09 +0700

    completed the project HomeWork2_Task10

--- HomeWork2/HomeWork2_Task10/Program.cs
+++ HomeWork2/HomeWork2_Task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number_user = 0;
+while (number_user < 100 || number_user >999)
+{
+ Console.Write("Введите трехзначное число ");
+ number_user = int.Parse(Console.ReadLine());
+ if(number_user < 100 && number_user > 999)
+ {
+    Console.WriteLine ("Вы ввели не трехзначное число");
+ }
+}
+int one_number = number_user/100;
+int three_number = number_user%10;
+int result = (number_user - (one_number*100+three_number))/10;
+Console.WriteLine($"Результат = {result}");
