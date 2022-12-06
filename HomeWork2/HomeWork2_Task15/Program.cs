4cfea0acbb10e64151905a5e8f95aa636a886453
Artyom <artyom_kudryavcev_91@mail.ru>
2022-12-07 01:20:05 +0700

    completed the project HomeWork2_Task15

--- HomeWork2/HomeWork2_Task15/Program.cs
+++ HomeWork2/HomeWork2_Task15/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number_day = 10;
+while (number_day < 1 | number_day > 7)
+{
+Console.Write("Введите порядковый номер дня недели: ");
+number_day = int.Parse(Console.ReadLine());
+Console.WriteLine("Введено некорректное число!!!");
+}
+if (number_day >= 6)
+{
+    Console.WriteLine("Введенный день недели выходной");
+}
+else 
+{
+    Console.WriteLine("Введенный день не выходной");
+}
