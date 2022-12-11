f8d0f82019a3e3ee2f714d05fa53f66787c2b29f
Artyom <artyom_kudryavcev_91@mail.ru>
2022-12-11 17:20:49 +0700

    completed the project HomeWork3_Task23

--- HomeWork3/HomeWork3_Task23/Program.cs
+++ HomeWork3/HomeWork3_Task23/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+Задание должно быть выполнено в методе и метод должен вернуть массив чисел.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int number = InputData();
+Console.WriteLine(number);
+
+int [] cubicNumbers = FindingSeriesOfNumbersAndRaisingCube(number);
+
+printArray(cubicNumbers);
+
+
+
+int InputData()
+{
+    bool stopCycle = false;
+    int inputNumber = -1;
+
+    while (!stopCycle)
+    {
+        Console.Write("Введите целое положительное число больше 0: ");
+        bool isParse = int.TryParse(Console.ReadLine(), out int inputNumber2);
+        if(isParse && inputNumber2 > 0)
+        {
+            stopCycle = true;
+            inputNumber = inputNumber2;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели некорректное число");
+            
+        }
+        
+    }
+    return inputNumber;
+    
+}
+
+int [] FindingSeriesOfNumbersAndRaisingCube (int imputNumber)
+{
+    int [] array = new int [imputNumber];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = (int)((Math.Pow(i+1,3)));
+    }
+    return array;
+}
+
+
+void printArray (int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+
+    }
+}
