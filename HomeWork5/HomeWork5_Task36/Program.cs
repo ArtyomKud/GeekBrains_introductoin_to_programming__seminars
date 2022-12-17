2ec88eeee24c651cc8cf22bd07496a81beaad7a4
Artyom <artyom_kudryavcev_91@mail.ru>
2022-12-18 02:03:38 +0700

    completed the project HomeWork5_Task36

--- HomeWork5/HomeWork5_Task36/Program.cs
+++ HomeWork5/HomeWork5_Task36/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных индексах.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0 */
+
+Random random = new Random();
+int numberMin = 10;
+int numberMax = 30;
+int size = 10;
+
+int [] array = GenerateArray(numberMin, numberMax, size);
+PrintArray(array);
+int sumNumbers = FindSumElementOfOddIndices(array);
+Console.WriteLine(sumNumbers);
+
+
+
+
+int [] GenerateArray (int numberMin1, int numberMax1, int size1)
+{
+    int [] arrayOut = new int [size1];
+    for (int i = 0; i < arrayOut.Length; i++)
+    {
+        arrayOut[i] = random.Next(numberMin, numberMax + 1);
+    }
+    return arrayOut;
+}
+
+void PrintArray (int [] array1)
+{
+    Console.WriteLine($"[{String.Join(" ,", array1)}]");
+}
+
+int FindSumElementOfOddIndices(int [] inputArray)
+{
+    int summOut = 0;
+    for (int i = 0; i <inputArray.Length; i++)
+    {
+        if (i%2 != 0)
+        {
+            summOut += inputArray[i];
+        }
+    }
+    return summOut;
+}
