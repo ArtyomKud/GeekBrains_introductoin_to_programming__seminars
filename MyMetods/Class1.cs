8bd3e8408f0339f9465b4b3a9f12dd3df5abb276
Artyom <artyom_kudryavcev_91@mail.ru>
2022-12-19 00:09:05 +0700

    added in project MyMetod metods: InputDataSeparatedSpaceOneLineDouble(String message), InputDataSeparatedSpaceOneLineInteger(String message)

--- MyMetods/Class1.cs
+++ MyMetods/Class1.cs
@@ -101,6 +101,168 @@ public static class Class1
     
     }
 
+    public static double [] InputDataSeparatedSpaceOneLineDouble(String message)
+    {
+         
+        Console.Write(message);
+    
+        String inputData = Console.ReadLine();
+        String temp;
+        String [] arrayString = new String [inputData.Length];
+        int count = 0;
+        char chack = ' ';
+        for(int i = 0; i < arrayString.Length; i++)
+        {
+            bool stop = true;
+        
+        
+            while (stop)
+            {
+                if (count >= inputData.Length)
+                {
+                    stop = false;
+                    break;
+                }
+            
+                if (inputData[count].Equals(chack))
+             {
+                
+                
+                    stop = false;
+            
+                
+        
+             }
+                else 
+                {
+                    temp = arrayString[i];
+                arrayString[i] = temp+inputData[count];
+
+                }
+                count++;
+            
+            }
+        
+        
+        
+        }
+    
+        int count2 = 0;
+        bool isParse;
+        for (int i = 0; i < arrayString.Length; i++)
+        {
+            isParse = double.TryParse(arrayString [i], out double inputNumber);
+            if (isParse)
+            {
+                count2++;
+            }
+        
+        }
+
+        double [] tempOutData = new double [count2];
+
+        for (int i = 0, j = 0; i < tempOutData.Length & j < arrayString.Length; i++, j++)
+        {
+            isParse = double.TryParse(arrayString [j], out double inputNumber);
+            if (isParse)
+            {
+                tempOutData[i] = inputNumber;
+                
+            }
+         else
+            {
+            j++;
+            isParse = double.TryParse(arrayString [j], out double inputNumber2);
+            tempOutData[i] = inputNumber2;
+            }
+        
+        }
+
+        return tempOutData; 
+    
+    }
+
+    public static int [] InputDataSeparatedSpaceOneLineInteger(String message)
+    {
+         
+        Console.Write(message);
+    
+        String inputData = Console.ReadLine();
+        String temp;
+        String [] arrayString = new String [inputData.Length];
+        int count = 0;
+        char chack = ' ';
+        for(int i = 0; i < arrayString.Length; i++)
+        {
+            bool stop = true;
+        
+        
+            while (stop)
+            {
+                if (count >= inputData.Length)
+                {
+                    stop = false;
+                    break;
+                }
+            
+                if (inputData[count].Equals(chack))
+             {
+                
+                
+                    stop = false;
+            
+                
+        
+             }
+                else 
+                {
+                    temp = arrayString[i];
+                arrayString[i] = temp+inputData[count];
+
+                }
+                count++;
+            
+            }
+        
+        
+        
+        }
+    
+        int count2 = 0;
+        bool isParse;
+        for (int i = 0; i < arrayString.Length; i++)
+        {
+            isParse = int.TryParse(arrayString [i], out int inputNumber);
+            if (isParse)
+            {
+                count2++;
+            }
+        
+        }
+
+        int [] tempOutData = new int [count2];
+
+        for (int i = 0, j = 0; i < tempOutData.Length & j < arrayString.Length; i++, j++)
+        {
+            isParse = int.TryParse(arrayString [j], out int inputNumber);
+            if (isParse)
+            {
+                tempOutData[i] = inputNumber;
+                
+            }
+         else
+            {
+            j++;
+            isParse = int.TryParse(arrayString [j], out int inputNumber2);
+            tempOutData[i] = inputNumber2;
+            }
+        
+        }
+
+        return tempOutData; 
+    
+    }
+
     public static int [] InputDataSeparatedSpaceOneLineInteger()
     {
          
@@ -194,6 +356,7 @@ public static class Class1
         }
         return count;
     }
+    
 
 
 }
