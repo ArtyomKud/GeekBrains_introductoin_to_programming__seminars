11ce2c1a72e2c6b386d38e985e41bba63a18fcb4
Artyom <artyom_kudryavcev_91@mail.ru>
2022-12-18 22:26:30 +0700

    completed the project HomeWork6_Task41

--- HomeWork6/HomeWork6_Task41/Program.cs
+++ HomeWork6/HomeWork6_Task41/Program.cs
@@ -1,1 +1,21 @@
-﻿using static MyMetods;
+﻿using static MyMetods.Class1;
+
+/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+
+
+
+
+int [] inputData = InputDataSeparatedSpaceOneLineInteger();
+
+
+PrintArray(inputData);
+
+int result = SummPositiveNumber(inputData);
+Console.WriteLine(result);
+
+    
