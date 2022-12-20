3e97156fb06e0a38cce38bec32cc00824dffae8d
Artyom <artyom_kudryavcev_91@mail.ru>
2022-12-21 00:32:22 +0700

    completed the project HomeWork7_Task47

--- HomeWork7/HomeWork7_Task47/Program.cs
+++ HomeWork7/HomeWork7_Task47/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static MyMetods.Class1;
+
+/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9 */
+int sizeMin = 1;
+int sizeMax = 50;
+
+int [] userDataSize2DArray = 
+InputDataSeparatedSpaceOneLineInteger("Введите количество строк и столбцов массива через пробел: ");
+PrintArray(userDataSize2DArray);
+try
+{
+    double[,] result2DArray = Generation2DArray(userDataSize2DArray, sizeMin, sizeMax);
+    PrintArray(result2DArray);
+    
+}
+catch (ArgumentException ex)
+{
+    
+    Console.WriteLine(ex.Message);
+}
+
